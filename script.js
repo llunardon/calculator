f33888ae2dd3d59d00237cb5ec20081702e361b3
llunardon <lorenzo.lunardon.1@gmail.com>
2021-03-14 22:16:47 +0100

    finished the base version, wired the JS

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const numButtons = document.querySelectorAll(".button-num");
+const opButtons = document.querySelectorAll(".button-op");
+const eqButton = document.querySelector(".button-eq");
+const clearButton = document.querySelector(".button-clear");
+const inputScreen = document.querySelector("#input-screen");
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+numButtons.forEach((button) => {
+    button.addEventListener("click", () => writeNumber(button.textContent));
+    return;
+});
+
+opButtons.forEach((button) => {
+    button.addEventListener("click", () => setOperator(button.textContent));
+    return;
+});
+
+eqButton.addEventListener("click", () => equals(operator, firstNumber));
+clearButton.addEventListener("click", () => clearScreen());
+
+//FUNCTIONS
+function writeNumber(number) {
+    inputScreen.textContent += number; 
+}
+
+function setOperator(inputOperator) {
+    firstNumber = inputScreen.textContent;
+    operator = inputOperator;
+    inputScreen.textContent = "";
+}
+
+function clearScreen() {
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = null;
+    inputScreen.textContent = "";
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -17,14 +58,20 @@ function divide(a, b) {
         return undefined;
 }
 
-function operator (op, a, b) {
+function equals (op, a) {
+    secondNumber = inputScreen.textContent;
+    inputScreen.textContent = "";
     switch (op) {
-        case "+": return add(a, b);
-        case "-": return sub(a, b);
-        case "*": return mul(a, b);
-        case "/": return divide(a, b);
-
+        case "+": inputScreen.textContent = add(Number(a), Number(secondNumber));
+            break;
+        case "-": inputScreen.textContent = sub(Number(a), Number(secondNumber));
+            break;
+        case "*": inputScreen.textContent = mul(Number(a), Number(secondNumber));
+            break;
+        case "/": inputScreen.textContent = divide(Number(a), Number(secondNumber));
+            break;
         default: return undefined;
+            break;
     };
+    firstNumber = 0;
 }
-
